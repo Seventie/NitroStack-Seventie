c45f555db5d52063a7a75ca3dc299a1d76fdeb01
seventie <abdussattarshaik07@gmail.com>
2026-07-26 05:55:36 +0530

    fix: import IngestionModule into PipelineModule to resolve missing DI provider for ParserService

--- src/modules/pipeline/pipeline.module.ts
+++ src/modules/pipeline/pipeline.module.ts
@@ -3,10 +3,11 @@ import { PipelineTools } from './pipeline.tools.js';
 import { RiskModule } from '../risk/risk.module.js';
 import { GraphModule } from '../graph/graph.module.js';
 import { RedactionModule } from '../redaction/redaction.module.js';
+import { IngestionModule } from '../ingestion/ingestion.module.js';
 
 @Module({
   name: 'pipeline',
-  imports: [RiskModule, GraphModule, RedactionModule],
+  imports: [RiskModule, GraphModule, RedactionModule, IngestionModule],
   providers: [PipelineTools],
   controllers: [PipelineTools]
 })
