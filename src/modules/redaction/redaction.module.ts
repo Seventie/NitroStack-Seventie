384d712fbcef066c764c882437e28ad593c99734
seventie <abdussattarshaik07@gmail.com>
2026-07-26 06:33:44 +0530

    fix: import IngestionModule into RedactionModule to fix DI crash

--- src/modules/redaction/redaction.module.ts
+++ src/modules/redaction/redaction.module.ts
@@ -4,9 +4,11 @@ import { ClassifierService } from './classifier.service.js';
 import { RedactionService } from './redaction.service.js';
 import { SessionVaultService } from './session-vault.service.js';
 import { NerClientService } from './ner.service.js';
+import { IngestionModule } from '../ingestion/ingestion.module.js';
 
 @Module({
   name: 'redaction',
+  imports: [IngestionModule],
   controllers: [RedactionTools],
   providers: [ClassifierService, RedactionService, SessionVaultService, NerClientService],
   exports: [ClassifierService, RedactionService, SessionVaultService, NerClientService]
